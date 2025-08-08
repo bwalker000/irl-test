0cba8527be81ffca5785a3108ca266cebdc619c8
bwalker000 <76655013+bwalker000@users.noreply.github.com>
2025-08-08 19:44:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ st.write(
 )
 
 st.button("Click me")
-st.download_button("Download file", data)
-st.link_button("Go to gallery", url)
+#st.download_button("Download file", data)
+#st.link_button("Go to gallery", url)
 st.page_link("app.py", label="Home")
 st.data_editor("Edit data", data)
 st.checkbox("I agree")
