ceabcb4d5406bda045eb7654577a49c996beab74
bwalker000 <bwalker000@gmail.com>
2025-08-22 11:52:08 -0400

    update submit

--- pages/12_Submit.py
+++ pages/12_Submit.py
@@ -6,7 +6,7 @@ responses = {}
 #st.write(type(st.session_state.QA))
 #st.write(st.session_state.QA)
 
-
+'''
 for dim in range(st.session_state.QA.shape[0]):
     for i, value in enumerate(st.session_state.QA[dim]):
         field_name = f"QA_{dim:02d}_{i}"
@@ -24,7 +24,7 @@ for dim, value in enumerate(st.session_state.TA):
 for dim, value in enumerate(st.session_state.TR):
     field_name = f"TR_{dim:02d}"
     responses[field_name] = value
-
+'''
 #responses["Venture"] = st.session_state.venture_id
 #responses["Project"] = st.session_state.project_id
 #responses["Support"] = st.session_state.support_id
@@ -36,14 +36,16 @@ airtable_date = today.isoformat()
 venture_name = st.session_state.venture_name
 project_name = st.session_state.project_name
 
+'''
 if st.session_state.mode == "ASSESSOR":
     responses["Name"] = venture_name + " - " + project_name + " - " + airtable_date
     responses["Assess_date"] = airtable_date
 elif st.session_state.mode == "REVIEWER":
     responses["Review_date"] = airtable_date
+    '''
 #
 #
-#
+
 api_key = st.secrets["general"]["airtable_api_key"]
 base_id = st.secrets["general"]["airtable_base_id"]
 table_name = st.secrets["general"]["airtable_table_data"]
