cb961767da5fdbedf6478627b147b018f4849d83
bwalker000 <bwalker000@gmail.com>
2025-08-25 17:21:31 -0400

    debug init_review

--- pages/2_Initiate_Review.py
+++ pages/2_Initiate_Review.py
@@ -49,7 +49,9 @@ air_reviewers, debug_details = load_airtable(table_name, base_id, api_key, debug
 
 # Determine the id and name of the support organization associated with the reviewer
 row = air_reviewers.loc[air_reviewers["id"] == st.session_state.reviewer_id[0]]
-st.session_state.support_id = row["Support Organizations"].tolist()
+
+# this comes out as a list. Normally we would add .tolist(), but not here.
+st.session_state.support_id = row["Support Organizations"]
 
 st.session_state.support_id
 
