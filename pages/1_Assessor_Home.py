cfbf1f8b7763b3da39d9425677f61b42a4a5b4c2
bwalker000 <bwalker000@gmail.com>
2025-08-28 14:40:39 -0400

    debug st.selectbox

--- pages/1_Assessor_Home.py
+++ pages/1_Assessor_Home.py
@@ -22,11 +22,18 @@ assessor_emails = df_assessors['fields.Email'].tolist()
 
 assessor_emails
 
+
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"),
+)
+
+
+
 # Streamlit selectbox for choosing a the Assessor
 ### *** In the future this will happen automatically at login.
 #assessor_email = st.selectbox('Select an Assessor:', options = assessor_emails)
 assessor_email = st.selectbox('Select an Assessor:', ("CAT", "dog", "FiSh"))
-
 
 if st.button("Assess"):
     st.session_state.assessor_email = assessor_email
