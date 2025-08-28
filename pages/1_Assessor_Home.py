615bdd193c1e4e27139d47ce3d67116b2b404ccc
bwalker000 <bwalker000@gmail.com>
2025-08-28 16:33:08 -0400

    debug pyairtable and streamlit

--- pages/1_Assessor_Home.py
+++ pages/1_Assessor_Home.py
@@ -20,20 +20,13 @@ air_assessors = air_assessors.all()
 df_assessors = pd.json_normalize(air_assessors)
 assessor_emails = df_assessors['fields.Email'].tolist()
 
-assessor_emails
-
-option = st.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
-)
-
+#assessor_emails
 
 # Streamlit selectbox for choosing a the Assessor
 ### *** In the future this will happen automatically at login.
-#assessor_email = st.selectbox('Select an Assessor:', options = assessor_emails)
+st.session_state.assessor_email = st.selectbox('Select an Assessor:', options = assessor_emails)
 
 if st.button("Assess"):
-#    st.session_state.assessor_email = assessor_email
     st.switch_page("pages/1_New_Assessment.py")
 if st.button("Home"):
     st.switch_page("streamlit_app.py")
