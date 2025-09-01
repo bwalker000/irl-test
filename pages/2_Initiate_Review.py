e72592c5af5bad2e7db29783adb1b38e1d7157ef
bwalker000 <bwalker000@gmail.com>
2025-09-01 08:41:21 -0400

    init_review

--- pages/2_Initiate_Review.py
+++ pages/2_Initiate_Review.py
@@ -87,10 +87,10 @@ elif st.session_state.review_mode == 0:
     table_name = st.secrets["general"]["airtable_table_data"]
     air_data = api.table(base_id, table_name)
 
-    st.session_state.support_id
+    st.session_state.support_id.tolist()
     st.write(pd.json_normalize(air_data.all()))
 
-    air_data_records = air_data.all(formula=match({"Support Organization": st.session_state.support_id}))
+    air_data_records = air_data.all(formula=match({"Support Organization": st.session_state.support_id.tolist()}))
 
     df_air_data_records = pd.json_normalize(air_data_records)
 
