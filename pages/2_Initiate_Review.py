620104c0a372ae8c4faef2f2d373a2a2f729ea17
bwalker000 <bwalker000@gmail.com>
2025-09-01 13:05:44 -0400

    init_review

--- pages/2_Initiate_Review.py
+++ pages/2_Initiate_Review.py
@@ -91,7 +91,7 @@ elif st.session_state.review_mode == 0:
 
     st.write(pd.json_normalize(air_data.all()))
 
-    air_data_records = air_data.all(formula = match({"Support Organization": st.session_state.support_id[0]}))
+    air_data_records = air_data.all(formula = match({"Support Organization": st.session_state.support_id[0][0]}))
 
 
 # prepare a list of existing assessments
