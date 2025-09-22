86c978b7e9c093787699af19cc8da2e8b8814cc7
bwalker000 <bwalker000@gmail.com>
2025-09-21 21:32:47 -0400

    update login

--- pages/0_Login.py
+++ pages/0_Login.py
@@ -6,11 +6,11 @@ if st.button("Home"):
     st.switch_page("streamlit_app.py")
 
 if st.button("Log in"):
-    st.login("auth0")
+    st.login()
 if st.user.is_logged_in:
     if st.button("Log out"):
         st.logout()
-st.write(f"Hello, {st.user.name}!")
+#st.write(f"Hello, {st.user.name}!")
              
 
 #if st.button("Login as Assessor"):
