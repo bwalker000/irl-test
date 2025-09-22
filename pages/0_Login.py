6bbef793257f02d96c18baa1233723a80ab38718
bwalker000 <bwalker000@gmail.com>
2025-09-21 21:29:18 -0400

    update login to use auth0

--- pages/0_Login.py
+++ pages/0_Login.py
@@ -2,9 +2,20 @@ from shared import *
 
 st.title("Login")
 
+st.login()
+
 if st.button("Home"):
     st.switch_page("streamlit_app.py")
-if st.button("Login as Assessor"):
-    st.switch_page("pages/1_Assessor_Home.py")
-if st.button("Login as Reviewer"):
-    st.switch_page("pages/2_Reviewer_Home.py")
+
+if st.button("Log in"):
+    st.login("auth0")
+if st.user.is_logged_in:
+    if st.button("Log out"):
+        st.logout()
+st.write(f"Hello, {st.user.name}!")
+             
+
+#if st.button("Login as Assessor"):
+#    st.switch_page("pages/1_Assessor_Home.py")
+#if st.button("Login as Reviewer"):
+#    st.switch_page("pages/2_Reviewer_Home.py")
