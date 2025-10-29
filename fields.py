3989aeb77379ac3bbfa69efff5d13a70932e6cfc
bwalker000 <bwalker000@gmail.com>
2025-10-29 08:19:19 -0400

    correct name error

--- fields.py
+++ fields.py
@@ -4,14 +4,14 @@ IRL_050_data_fields = ["id", "Name", "Venture", "Project", "ASSESSOR", "Assess_d
 for i in range(0, 16):
     for j in range(0, 10):
         string = f"QA_{i:02}_{j}"
-        IRL_031_data_fields.append(string)
+        IRL_050_data_fields.append(string)
         string = f"QR_{i:02}_{j}"
-        IRL_031_data_fields.append(string)
+        IRL_050_data_fields.append(string)
 
 for i in range(0, 16):
     string = f"TA_{i:02}"
-    IRL_031_data_fields.append(string)
+    IRL_050_data_fields.append(string)
     string = f"TR_{i:02}"
-    IRL_031_data_fields.append(string)
+    IRL_050_data_fields.append(string)
 #---------------------------------------------------------------------------------
 
