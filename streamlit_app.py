765bd818fa71dcdb96810f0ef07885baed2b034d
bwalker000 <bwalker000@gmail.com>
2026-01-10 13:06:41 -0500

    debug login

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ from shared import *
 # Set page config for wide mode - must be first Streamlit command
 st.set_page_config(layout="wide")
 
-display_logo()
+# Temporarily disable logo to fix auth callback issue
+# display_logo()
 
 st.title("Impact Readiness Level")
 
